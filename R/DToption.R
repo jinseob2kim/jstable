d6643bcca36bf0131fcf1fd69c2c021260a6bc15
Jinseob Kim <jinseob2kim@gmail.com>
2018-05-28 02:14:45 +0000

    DToption

---
+++ R/DToption.R
@@ -0,0 +1,134 @@
+
+#' @title datable option for data(DT package)
+#' @description DT::datatable option for data
+#' @param fname File name to download
+#' @return datatable object
+#' @details DETAILS
+#' @examples 
+#' \dontrun{
+#' if(interactive()){
+#'  datatable(a.label, rownames=F, extension= "Buttons", caption = "Labels of data",
+#'  options = opt.data("data")
+#'  )
+#'  }
+#' }
+#' @rdname opt.data
+#' @export 
+
+opt.data = function(fname){
+  return(
+    list(dom = '<lf<rt>Bip>', lengthMenu = list(c(10, 25, -1), c('10', '25', 'All')), pageLength = 10,
+         buttons = list('copy', 
+                        'print', 
+                        list(extend = 'collection', 
+                             buttons = list(list(extend = 'csv', filename= fname),
+                                            list(extend = 'excel', filename= fname), 
+                                            list(extend = 'pdf', filename= fname)
+                             ), 
+                             text = 'Download')
+         )
+    )
+  )
+}
+
+
+#' @title datable option for table 1(DT package)
+#' @description DT::datatable option for table 1
+#' @param fname File name to download
+#' @return datatable object
+#' @details DETAILS
+#' @examples 
+#' \dontrun{
+#' if(interactive()){
+#'  datatable(ptb1, rownames=T, extension= "Buttons", caption = cap.tb1,
+#'  options = c(opt.tb1("tb1"), list(columnDefs = list(list(visible=FALSE, targets=ncol(ptb1)))
+#'                                  )
+#'             )
+#'  }
+#' }
+#' @rdname opt.tb1
+#' @export 
+
+opt.tb1 = function(fname){
+  return(
+    list(dom = '<lf<rt>Bip>', lengthMenu = list(c(10, 25, -1), c('10', '25', 'All')), pageLength = 25, ordering=F,
+         buttons = list('copy', 
+                        'print', 
+                        list(extend = 'collection', 
+                             buttons = list(list(extend = 'csv', filename= fname),
+                                            list(extend = 'excel', filename= fname), 
+                                            list(extend = 'pdf', filename= fname)
+                             ), 
+                             text = 'Download')
+         )
+    )
+  )
+}
+
+
+
+#' @title datable option for regression table(DT package)
+#' @description DT::datatable option for glm, gee(geepack packge), lmer/glmer(lme4 package)
+#' @param fname File name to download
+#' @return datatable object
+#' @details DETAILS
+#' @examples 
+#' \dontrun{
+#' if(interactive()){
+#'  datatable(out.gee, rownames=T, extension= "Buttons", caption = cap.gee,
+#'           options = c(opt.tbreg(paste("gee_gaussian", y , paste(xs, collapse = "_"), sep="_"))
+#'                      )
+#'           )
+#'  }
+#' }
+#' @rdname opt.tbreg
+#' @export 
+
+opt.tbreg = function(fname){
+  return(
+    list(dom = '<lf<rt>Bip>', lengthMenu = list(c(10, 25, -1), c('10', '25', 'All')), pageLength = -1, ordering=F,
+         buttons = list('copy', 
+                        'print', 
+                        list(extend = 'collection', 
+                             buttons = list(list(extend = 'csv', filename= fname),
+                                            list(extend = 'excel', filename= fname), 
+                                            list(extend = 'pdf', filename= fname)
+                             ), 
+                             text = 'Download')
+         )
+    )
+  )
+}
+
+#' @title datable option for ROC result(DT package)
+#' @description DT::datatable option for ROC result
+#' @param fname File name to download
+#' @return datatable object
+#' @details DETAILS
+#' @examples 
+#' \dontrun{
+#' if(interactive()){
+#'  datatable(roc.table, rownames=F, extension= "Buttons", caption = "Best cut-off", 
+#'            options = opt.roc(paste("roc", input$dep_vars_roc , 
+#'                                    paste(xs = input$indep_vars_roc, collapse = "_"), sep="_"))
+#'            )
+#'  }
+#' }
+#' @rdname opt.roc
+#' @export 
+
+opt.roc = function(fname){
+  return(
+    list(dom = '<<rt>Bip>', ordering=F,
+         buttons = list('copy', 
+                        'print', 
+                        list(extend = 'collection', 
+                             buttons = list(list(extend = 'csv', filename= fname),
+                                            list(extend = 'excel', filename= fname), 
+                                            list(extend = 'pdf', filename= fname)
+                             ), 
+                             text = 'Download')
+         )
+    )
+  )
+}
